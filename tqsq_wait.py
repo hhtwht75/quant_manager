68e4331f2db8a4047df6b21d31558f895c1698b7
hhtwht75 <hhtwht75@gmail.com>
2023-10-06 15:15:44 +0900

    테슬라만 되는걸로

--- tqsq_wait.py
+++ tqsq_wait.py
@@ -4,24 +4,24 @@ import random
 import numpy as np  # 표준편차 계산을 위한 라이브러리
 
 # 데이터 다운로드
-start_date = '2022-09-01'
+start_date = '2022-10-01'
 end_date = '2023-09-01'
-long_ticker = 'TSLA'
-short_ticker = 'TSLS'
+long_ticker = 'GOOGL'
+short_ticker = 'GGLS'
 full_long_data = yf.download(long_ticker, start=start_date, end=end_date)
 full_short_data = yf.download(short_ticker, start=start_date, end=end_date)
 
 # 초기 설정
 gain_threshold = 0.01  # 상승
 transaction_fee_rate = 0.0005  # 수수료
-iterations = 100
+iterations = 10
 
 profits = []
 max_losses = []
 
 for _ in range(iterations):
     # 랜덤한 시작 날짜와 랜덤한 구간 길이 선택
-    random_length = 60
+    random_length = 30
     # random_length = random.randint(30, 100)
 
     random_start_index = random.randint(0, len(full_long_data) - random_length - 1)
@@ -44,13 +44,13 @@ for _ in range(iterations):
             cash -= purchased_shares * purchase_price_long * (1 + transaction_fee_rate)
             cash += purchased_shares * long.loc[date, 'Close'] * (1 - transaction_fee_rate)
 
-        # short position에 대한 로직
-        purchase_price_short = short.loc[date, 'Open'] * (1 + gain_threshold)
-        if short.loc[date, 'High'] >= purchase_price_short:
-            buy_amount = (0.5 * cash) / (1 + transaction_fee_rate)
-            purchased_shares = buy_amount // purchase_price_short
-            cash -= purchased_shares * purchase_price_short * (1 + transaction_fee_rate)
-            cash += purchased_shares * short.loc[date, 'Close'] * (1 - transaction_fee_rate)
+        # # short position에 대한 로직
+        # purchase_price_short = short.loc[date, 'Open'] * (1 + gain_threshold)
+        # if short.loc[date, 'High'] >= purchase_price_short:
+        #     buy_amount = (0.5 * cash) / (1 + transaction_fee_rate)
+        #     purchased_shares = buy_amount // purchase_price_short
+        #     cash -= purchased_shares * purchase_price_short * (1 + transaction_fee_rate)
+        #     cash += purchased_shares * short.loc[date, 'Close'] * (1 - transaction_fee_rate)
 
     # 각 랜덤 데이터 구간의 마지막 날짜에서 수익률과 최대 손실 계산
     profits.append((cash - portfolio_value) / portfolio_value)
