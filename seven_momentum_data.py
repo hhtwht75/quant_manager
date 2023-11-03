a8fa079c787dfb28be9c6ccdb89faa860cae5568
hhtwht75 <hhtwht75@gmail.com>
2023-11-03 12:52:09 +0900

    1m test

--- seven_momentum_data.py
+++ seven_momentum_data.py
@@ -80,6 +80,7 @@ def backtest_strategy(tickers, stock_data, initial_capital=100000, stop_loss=0.0
 # whole_tickers = ["TQQQ", "SQQQ", "TMV", "TMf", "TYO", "TYD", "YANG", "YINN", "EDZ", "EDC", "TZA", "TNA", "WEBL", "WEBS", "FAZ", "FAS", "DRV", "DRN", "HIBS", "HIBL", "LABD", "LABU", "SOXL", "SOXS", "TECL", "TECS"]
 # whole_tickers = ["TQQQ", "SQQQ", "LABD", "LABU", "SOXL", "SOXS", "TECL", "TECS", "FNGU", "FNGD"]
 whole_tickers = ["LABD", "LABU", "SOXL", "SOXS"]
+# whole_tickers = ["SOXL", "SOXS"]
 # whole_tickers = ["TYD", "TYO"]
 
 
@@ -90,8 +91,8 @@ for i in range(0,1):
     
     interval = "15m"
     # period = "20d"
-    start_date = "2023-09-02"
-    end_date = "2023-11-01"
+    start_date = "2023-10-10"
+    end_date = "2023-11-03"
     initial_capital = 10000
     stop_loss = 0.010
     commission_rate = 0.001
